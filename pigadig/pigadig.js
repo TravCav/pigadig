9e39d71601bff55cbff5784bec93c53f6c1a349c
TravCav <xurrux@gmail.com>
2018-05-22 23:35:36 -0400

    things

--- pigadig/pigadig.js
+++ pigadig/pigadig.js
@@ -1,12 +1,43 @@
-////var Items = require('./items');
 const items = [
-    { name: 'hydrogen', time: 5, dependencies: [] },
-    { name: 'helium', time: 10, dependencies: [] }];
+    { name: 'hydrogen', time: 1, dependencies: [] },
+    { name: 'helium', time: 1, dependencies: [] },
+    { name: 'oxygen', time: 1, dependencies: [] },
+    { name: 'water', time: 10, dependencies: [{item: "hydrogen", qty: 2}, {item: "oxygen", qty: 1}] },
+    
+];
 
 var inventory = [];
 var gathering = false;
+var timeSpent = 0;
 
-function FindItem(itemName) {
+function AddToInventory(newItem){
+    let alreadyHave = false;
+    inventory.forEach(item => {
+        if(item.name == newItem.name){
+            item.qty++;
+            alreadyHave = true;
+        }
+    });
+
+    if(!alreadyHave){
+        inventory.push({name: newItem.name, qty: 1});
+    }
+}
+
+function RemoveFromInventory(newItem){
+    for(let index = 0; index < inventory.length; index++) {
+        let item = inventory[index];
+        if(item.name == newItem.name) {
+            if(item.qty > 0){
+                item.qty--;
+            } else {
+                inventory.splice(1, 1);
+            }
+        }
+    }
+}
+
+function LookupItem(itemName) {
     for (let index = 0; index < items.length; index++) {
         const item = items[index];
         if(item.name === itemName) {
@@ -14,25 +45,56 @@ function FindItem(itemName) {
         }
     }
 }
-function DoneGathering(wat) {
-    gathering = false;
-    console.log(wat);
-    return;
+
+function MakeItem(itemName) {
+    const item = LookupItem(itemName);
+    let canMakeItem = false;
+
+    // does it have any requirements
+    if (item.dependencies.length === 0) {
+        canMakeItem = true;
+    } else {
+        let missingItems = false;
+        // can we meet the requirements
+        item.dependencies.forEach(dependency => {
+            missingItems |= !HaveEnough(dependency);            
+        });
+        
+        console.log("Has All Items: ", !missingItems);
+    }
+
+    // We meet the requirements. Make the Item.
+    ////console.log('Making: ', item.name);
+    timeSpent += item.time;
+    AddToInventory(item);
 }
 
-function GatherItem(itemName) {
-    const item = FindItem(itemName);
-    console.log('Gathering: ', item.name);
-    gathering = true;
-    setTimeout(DoneGathering(1), item.time);
+function HaveEnough(dependency) {
+    ////console.log("checking for: ", dependency);
+    let haveEnough = false;
+    
+    inventory.forEach(item => {
+        ////console.log(item.name, dependency.item);
+        if(item.name === dependency.item) {
+            ////console.log("has " + item.qty + " " + item.name);
+            if(item.qty < dependency.qty) {
+                console.log("need " + (dependency.qty - item.qty) + " more " + dependency.item);
+            } else {
+                haveEnough = true;
+            }
+        }
+    });
 
-    console.log('Done gathering: ', item.name);
+    return haveEnough;
 }
-
 
 console.log("And here our adventure begins...");
 
-GatherItem("hydrogen");
+MakeItem("hydrogen");
+MakeItem("hydrogen");
+MakeItem("oxygen");
+MakeItem("water");
 
-
+console.log("Finished with: \r\n", inventory);
+console.log("Time Spent: ", timeSpent);
 console.log("Farewell");
