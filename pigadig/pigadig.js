11aef81afa4ea889c9ce0736d2790a12aca6613a
cav7ay <jesse.cavanaugh@cchmc.org>
2018-05-23 16:56:48 -0400

    more fun

--- pigadig/pigadig.js
+++ pigadig/pigadig.js
@@ -1,138 +1,177 @@
 const items = [{
-        name: 'hydrogen',
-        time: 1,
-        dependencies: []
-    },
-    {
-        name: 'helium',
-        time: 1,
-        dependencies: []
-    },
-    {
-        name: 'oxygen',
-        time: 1,
-        dependencies: []
-    },
-    {
-        name: 'water',
-        time: 10,
-        dependencies: [{
-            item: "hydrogen",
-            qty: 2
-        }, {
-            item: "oxygen",
-            qty: 1
-        }]
-    },
-
+    name: "hydrogen",
+    time: 1,
+    dependencies: []
+  },
+  {
+    name: "helium",
+    time: 1,
+    dependencies: []
+  },
+  {
+    name: "oxygen",
+    time: 1,
+    dependencies: []
+  },
+  {
+    name: "water",
+    time: 10,
+    dependencies: [{
+        item: "hydrogen",
+        qty: 2
+      },
+      {
+        item: "oxygen",
+        qty: 1
+      }
+    ]
+  }
 ];
+let player = {
+  inventory: [],
+  timeSpent: 0
+};
 
-var inventory = [];
-var gathering = false;
-var timeSpent = 0;
+function AddToInventory(newItem, qty) {
+  let alreadyHave = false;
+  player.inventory.forEach(item => {
+    if (item.name == newItem.name) {
+      item.qty += qty;
+      alreadyHave = true;
+    }
+  });
 
-function AddToInventory(newItem) {
-    let alreadyHave = false;
-    inventory.forEach(item => {
-        if (item.name == newItem.name) {
-            item.qty++;
-            alreadyHave = true;
-        }
+  if (!alreadyHave) {
+    player.inventory.push({
+      name: newItem.name,
+      qty: qty
     });
-
-    if (!alreadyHave) {
-        inventory.push({
-            name: newItem.name,
-            qty: 1
-        });
-    }
+  }
 }
 
-function RemoveDependencyFromInventory(dependency) {
-    ////console.log("remove: ", dependency);
-    for (let index = 0; index < inventory.length; index++) {
-        let item = inventory[index];
-        if (item.name == dependency.item) {
-            if (item.qty >= dependency.qty) {
-                item.qty -= dependency.qty;
-                if (item.qty === 0) {
-                    // that was the last one.
-                    inventory.splice(index, 1);
-                }
-            } else {
-                console.log("don't have enough of those.");
-            }
-        }
-    }
-}
+function DeconstructItem(itemName) {
+  const item = LookupItem(itemName);
 
-function LookupItem(itemName) {
-    for (let index = 0; index < items.length; index++) {
-        const item = items[index];
-        if (item.name === itemName) {
-            return item;
-        }
-    }
-}
+  if (item.dependencies.length > 0) {
+    let itemRemoved = RemoveItemsFromInventory(itemName, 1);
+    if (itemRemoved) {
+      item.dependencies.forEach(dependency => {
+        AddToInventory(LookupItem(dependency.item), dependency.qty);
+      });
 
-function MakeItem(itemName) {
-    const item = LookupItem(itemName);
-    let canMakeItem = false;
-
-    // does it have any requirements
-    if (item.dependencies.length === 0) {
-        canMakeItem = true;
+      player.timeSpent += item.time / 2;
     } else {
-        let missingItems = false;
-        // can we meet the requirements
-        item.dependencies.forEach(dependency => {
-            missingItems |= !HaveEnough(dependency);
-        });
-
-        ////console.log("Has All Items: ", !missingItems);
-        if (!missingItems) {
-            item.dependencies.forEach(dependency => {
-                RemoveDependencyFromInventory(dependency);
-            });
-
-            canMakeItem = true;
-        }
+      console.log('You dig around in your pockets but, can\'t seem to find any ' + itemName);
     }
+  } else {
+    console.log("You try to break it but, nothing happens.");
+  }
+}
 
-    // We meet the requirements. Make the Item.
-    if (canMakeItem) {
-        ////console.log('Making: ', item.name);
-        timeSpent += item.time;
-        AddToInventory(item);
-    }
+function GoAdventuring() {
+  const randomItem = items[Math.floor(Math.random() * items.length)];
+  AddToInventory(randomItem, 1);
+  player.timeSpent += 10;
+  console.log('You wander aimlessly, but find no adventuring to be had. You did happen to find some ' + randomItem.name + ' though.');
 }
 
 function HaveEnough(dependency) {
-    ////console.log("checking for: ", dependency);
-    let haveEnough = false;
+  ////console.log("checking for: ", dependency);
+  let haveEnough = false;
 
-    inventory.forEach(item => {
-        ////console.log(item.name, dependency.item);
-        if (item.name === dependency.item) {
-            ////console.log("has " + item.qty + " " + item.name);
-            if (item.qty < dependency.qty) {
-                console.log("need " + (dependency.qty - item.qty) + " more " + dependency.item);
-            } else {
-                haveEnough = true;
-            }
-        }
+  player.inventory.forEach(item => {
+    ////console.log(item.name, dependency.item);
+    if (item.name === dependency.item) {
+      ////console.log("has " + item.qty + " " + item.name);
+      if (item.qty < dependency.qty) {
+        console.log(
+          "need " + (dependency.qty - item.qty) + " more " + dependency.item
+        );
+      } else {
+        haveEnough = true;
+      }
+    }
+  });
+
+  return haveEnough;
+}
+
+function LookupItem(itemName) {
+  for (let index = 0; index < items.length; index++) {
+    const item = items[index];
+    if (item.name === itemName) {
+      return item;
+    }
+  }
+}
+
+function MakeItem(itemName) {
+  const item = LookupItem(itemName);
+  let canMakeItem = false;
+
+  // does it have any requirements
+  if (item.dependencies.length === 0) {
+    canMakeItem = true;
+  } else {
+    let missingItems = false;
+    // can we meet the requirements
+    item.dependencies.forEach(dependency => {
+      missingItems |= !HaveEnough(dependency);
     });
 
-    return haveEnough;
+    ////console.log("Has All Items: ", !missingItems);
+    if (!missingItems) {
+      item.dependencies.forEach(dependency => {
+        RemoveItemsFromInventory(dependency.item, dependency.qty);
+      });
+
+      canMakeItem = true;
+    }
+  }
+
+  // We meet the requirements. Make the Item.
+  if (canMakeItem) {
+    ////console.log('Making: ', item.name);
+    player.timeSpent += item.time;
+    AddToInventory(item, 1);
+  }
+}
+
+function RemoveItemsFromInventory(itemName, qty) {
+  for (let index = 0; index < player.inventory.length; index++) {
+    let item = player.inventory[index];
+    if (item.name == itemName) {
+      if (item.qty >= qty) {
+        item.qty -= qty;
+        if (item.qty === 0) {
+          // that was the last one.
+          player.inventory.splice(index, 1);
+
+        }
+
+        return true;
+      } else {
+        console.log("don't have enough of those.");
+
+        return false;
+      }
+    }
+  }
 }
 
 console.log("And here our adventure begins...");
 
-MakeItem("hydrogen");
-MakeItem("hydrogen");
-MakeItem("oxygen");
+for (let index = 0; index < 10; index++) {
+  GoAdventuring();
+}
+
+// // MakeItem("hydrogen");
+// // MakeItem("hydrogen");
+// // MakeItem("oxygen");
 MakeItem("water");
 
-console.log("Finished with: \r\n", inventory);
-console.log("Time Spent: ", timeSpent);
+DeconstructItem("water");
+
+console.log("Finished with: \r\n", player.inventory);
+console.log("Time Spent: ", player.timeSpent);
 console.log("Farewell");
