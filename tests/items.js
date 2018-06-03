90fb60f8facbd9ccdcb7b36a9e89cb8d19574538
TravCav <xurrux@gmail.com>
2018-06-02 23:30:12 -0400

    tests

--- tests/items.js
+++ tests/items.js
@@ -4,16 +4,22 @@ let Items = require('../src/items');
 
 
 
-describe('Items', function () {
-    describe('Give Items', function () {
-
-        it('should have 2 water', function () {
-            let player = new Entity.Entity({
-                name: "player",
-                hp: 10
-            });
-            Items.GiveItems(player, 'water', 2);
-            assert.equal(player.HasItems('water', 2), true);
+describe('Entity', function () {
+    it('should have 2 water', function () {
+        let player = new Entity.Entity({
+            name: "testplayer"
         });
+        Items.GiveItems(player, 'water', 2);
+        assert.equal(player.HasItems('water', 2), true);
+    });
+
+    it('should be able to deconstruct item', function () {
+        let player = new Entity.Entity({
+            name: "testplayer"
+        });
+        Items.GiveItems(player, 'water', 2);
+        player.DeconstructItem('water');
+        assert.equal(player.HasItems('hydrogen', 2), true);
+        assert.equal(player.HasItems('oxygen', 1), true);
     });
 });
